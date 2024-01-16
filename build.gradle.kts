092977b0efb93a43ee66b027aa6eb4a7e1677b77
nayunjae <yunjae.na@wantedlab.com>
2024-01-16 11:26:10 +0900

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.io.FileInputStream
 import java.util.Properties
 
 fun properties(key: String) = providers.gradleProperty(key)
-fun environment(key: String) = providers.environmentVariable(key)
+fun environment(key: String) = System.getenv(key)
 val props = Properties().apply {
     try {
         load(FileInputStream(rootProject.file("private.properties")))
@@ -126,14 +126,14 @@ tasks {
     }
 
     signPlugin {
-        certificateChain = environment("CERTIFICATE_CHAIN").getOrElse(props.getProperty("CERTIFICATE_CHAIN").trimIndent())
-        privateKey = environment("PRIVATE_KEY").getOrElse(props.getProperty("PRIVATE_KEY").trimIndent())
-        password = environment("PRIVATE_KEY_PASSWORD").getOrElse((props.getProperty("PRIVATE_KEY_PASSWORD").trimIndent()))
+        certificateChain = environment("CERTIFICATE_CHAIN") ?: props.getProperty("CERTIFICATE_CHAIN").trimIndent()
+        privateKey = environment("PRIVATE_KEY") ?: props.getProperty("PRIVATE_KEY").trimIndent()
+        password = environment("PRIVATE_KEY_PASSWORD") ?: props.getProperty("PRIVATE_KEY_PASSWORD").trimIndent()
     }
 
     publishPlugin {
         dependsOn("patchChangelog")
-        token = environment("PUBLISH_TOKEN").getOrElse(props.getProperty("PUBLISH_TOKEN"))
+        token = environment("PUBLISH_TOKEN") ?: props.getProperty("PUBLISH_TOKEN")
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
