40dbfc96976086707c74102d04cd25bdd0a6f6c5
nayunjae <yunjae.na@wantedlab.com>
2024-01-16 10:44:32 +0900

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,11 @@ import java.util.Properties
 fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
 val props = Properties().apply {
-    load(FileInputStream(rootProject.file("private.properties")))
+    try {
+        load(FileInputStream(rootProject.file("private.properties")))
+    } catch (_: Exception) {
+
+    }
 }
 
 plugins {
@@ -47,7 +51,9 @@ intellij {
 
     //intellij.localPath.set(properties("StudioRunPath")) //localPath 를 추가 해줘야 intellij가 아닌 Android Studio 가 열림
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+    plugins = properties("platformPlugins").map {
+        it.split(',').map(String::trim).filter(String::isNotEmpty)
+    }
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
@@ -86,17 +92,19 @@ tasks {
         sinceBuild = properties("pluginSinceBuild")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-            val start = "<!-- Plugin description -->"
-            val end = "<!-- Plugin description end -->"
+        pluginDescription =
+            providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
 
-            with (it.lines()) {
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                with(it.lines()) {
+                    if (!containsAll(listOf(start, end))) {
+                        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                    }
+                    subList(indexOf(start) + 1, indexOf(end)).joinToString("\n")
+                        .let(::markdownToHTML)
                 }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
             }
-        }
 
         val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
@@ -122,9 +130,14 @@ tasks {
     }
 
     signPlugin {
-        certificateChain = environment("CERTIFICATE_CHAIN").getOrElse(props.getProperty("CERTIFICATE_CHAIN").trimIndent())
-        privateKey = environment("PRIVATE_KEY").getOrElse(props.getProperty("PRIVATE_KEY").trimIndent())
-        password = environment("PRIVATE_KEY_PASSWORD").getOrElse((props.getProperty("PRIVATE_KEY_PASSWORD").trimIndent()))
+        certificateChain = environment("CERTIFICATE_CHAIN").getOrElse(
+            props.getProperty("CERTIFICATE_CHAIN").trimIndent()
+        )
+        privateKey =
+            environment("PRIVATE_KEY").getOrElse(props.getProperty("PRIVATE_KEY").trimIndent())
+        password = environment("PRIVATE_KEY_PASSWORD").getOrElse(
+            (props.getProperty("PRIVATE_KEY_PASSWORD").trimIndent())
+        )
     }
 
     publishPlugin {
@@ -133,6 +146,8 @@ tasks {
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels = properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
+        channels = properties("pluginVersion").map {
+            listOf(it.split('-').getOrElse(1) { "default" }.split('.').first())
+        }
     }
 }
